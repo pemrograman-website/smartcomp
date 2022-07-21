3ad95977dde0a7cae1f245b8101fa5dc1719a051
Yusuf Yudhistira <yudhis96@yahoo.com>
2022-07-21 21:43:43 +0700

    Set ekstension Kartik-V agar pakai BS 4, karena BS4 sudah bundle dengan AdminLTE

--- backend/config/params.php
+++ backend/config/params.php
@@ -1,4 +1,8 @@
 <?php
 return [
     'adminEmail' => 'admin@example.com',
+
+    'bsVersion' => '4.x', // this will set globally `bsVersion` to Bootstrap 4.x for all Krajee Extensions
+    'bsDependencyEnabled' => false, // this will not load Bootstrap CSS and JS for all Krajee extensions
+    // you need to ensure you load the Bootstrap CSS/JS manually in your view layout before Krajee CSS/JS assets
 ];
